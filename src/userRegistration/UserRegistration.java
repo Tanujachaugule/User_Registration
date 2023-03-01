20c6381ecbff2f4b9790d64206e18f81a54b831e
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-01 20:30:54 +0530

    Added: Enter Password At least minimum 8 Characters

--- src/userRegistration/UserRegistration.java
+++ src/userRegistration/UserRegistration.java
@@ -22,6 +22,10 @@ public class UserRegistration {
         System.out.println("Enter Your Mobile Number");
         String mobileNo = scanner.next();
         userregistration.validMobileFormat(mobileNo);
+        System.out.println();
+        System.out.println("\nEnter Password");
+        String password = scanner.next();
+        userregistration.validPassword(password);
     }
     public void validFirstName(String firstName) {
         String pattern ="^[A-Z]{1}[ a-z]{2,25}$";
@@ -53,6 +57,14 @@ public class UserRegistration {
             System.out.println("Mobile Number  is Valid");
         }else {
             System.out.println("Mobile Number is Invalid");
+        }
+    }
+    public void validPassword(String password) {
+        String pattern = "[a-zA-z]{8,32}$";
+        if(Pattern.matches(pattern, password)) {
+            System.out.println("Password  is Valid");
+        }else {
+            System.out.println("Password is Invalid");
         }
     }
 }
